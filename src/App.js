6dca925501371d746f30626c227061cf97738de1
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-12-16 00:50:37 +0000

    Trying to get primer react to work in night mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { ThemeProvider, Box, BaseStyles, Heading } from '@primer/react'
 
 // Components
 import Navbar from './components/Navbar'
@@ -7,11 +8,23 @@ import Navbar from './components/Navbar'
 import './styles/App.css'
 
 function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  )
+    return (
+        <ThemeProvider colorMode="night">
+            <BaseStyles>
+                <Box display="grid" gridTemplateColumns="1fr 1fr" gridGap={3}>
+                    <Box p={3} borderColor="border.default" borderWidth={1} borderStyle="solid">
+                        1
+                    </Box>
+                    <Box p={3} borderColor="border.default" borderWidth={1} borderStyle="solid">
+                        2
+                    </Box>
+                    <Box p={3} borderColor="border.default" borderWidth={1} borderStyle="solid">
+                        3
+                    </Box>
+                </Box>
+            </BaseStyles>
+        </ThemeProvider>
+    )
 }
 
 export default App
